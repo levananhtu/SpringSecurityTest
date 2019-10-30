4437c898570eb64a3550432fb44725eeeda9e0e4
dvs-tulva <56066024+dvs-tulva@users.noreply.github.com>
2019-10-30 08:45:52 +0700

    checkpoint02 master

--- src/main/java/com/lvat/SpringSecurityTest/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lvat.SpringSecurityTest.repository;
-
-import com.lvat.SpringSecurityTest.login.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByRole(String role);
-}
